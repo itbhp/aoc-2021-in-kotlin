139941913941944f3db9563edf577c2d18a92cf4
Paolo Sciarra <itbhps@gmail.com>
2021-12-01 19:48:57 +0100

    [aoc-2021] day1 part2 solved

--- src/main/kotlin/it/twinsbrain/aco/kotlin/Day01.kt
+++ src/main/kotlin/it/twinsbrain/aco/kotlin/Day01.kt
@@ -1,20 +1,34 @@
 package it.twinsbrain.aco.kotlin
 
+import it.twinsbrain.aco.kotlin.Day01.howManyIncreases
+import it.twinsbrain.aco.kotlin.Day01.howManySubsequentIncreases
 import it.twinsbrain.aco.kotlin.common.FileModule.readInput
 
 object Day01 {
   // part1
-  fun howManySubsequentIncreases(input: List<Int>): Int {
-    val pairs: List<Pair<Int, Int>> = input.take(input.size - 1).zip(input.subList(1, input.size))
-    return pairs.count { p ->
-      val (prev, next) = p
-      prev < next
+  fun howManySubsequentIncreases(input: List<Int>): Int =
+    input
+      .take(input.size - 1)
+      .zip(input.drop(1))
+      .count { (prev, next) -> prev < next }
+
+  // part2
+  fun howManyIncreases(input: List<Int>): Int {
+    val compacted = mutableListOf<Int>()
+    for (i in 0..input.size - 2) {
+      if (i < input.size - 2) {
+        compacted.add(input[i] + input[i + 1] + input[i + 2])
+      } else {
+        break
+      }
     }
+    return howManySubsequentIncreases(compacted)
   }
 }
 
 fun main() {
   val day1Input = readInput("/inputs/day1.txt")
     .map { it.toInt() }
-  println(Day01.howManySubsequentIncreases(day1Input))
+  println(howManySubsequentIncreases(day1Input))
+  println(howManyIncreases(day1Input))
 }
--- src/test/kotlin/it/twinsbrain/aco/kotlin/Day01Test.kt
+++ src/test/kotlin/it/twinsbrain/aco/kotlin/Day01Test.kt
@@ -1,29 +1,53 @@
 package it.twinsbrain.aco.kotlin
 
+import it.twinsbrain.aco.kotlin.Day01.howManyIncreases
 import it.twinsbrain.aco.kotlin.Day01.howManySubsequentIncreases
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class Day01Test {
 
-  @Test
-  internal fun zeroIncreases() {
-    assertThat(
-      howManySubsequentIncreases(listOf(1, 1, 1, 1))
-    ).isEqualTo(0)
+  @Nested
+  class Part1Test {
+    @Test
+    internal fun zeroIncreases() {
+      assertThat(
+        howManySubsequentIncreases(listOf(1, 1, 1, 1))
+      ).isEqualTo(0)
+    }
+
+    @Test
+    internal fun oneIncrease() {
+      assertThat(
+        howManySubsequentIncreases(listOf(1, 1, 2, 2))
+      ).isEqualTo(1)
+    }
+
+    @Test
+    internal fun multipleIncreases() {
+      assertThat(
+        howManySubsequentIncreases(listOf(1, 1, 2, 3))
+      ).isEqualTo(2)
+    }
   }
 
-  @Test
-  internal fun oneIncrease() {
-    assertThat(
-      howManySubsequentIncreases(listOf(1, 1, 2, 2))
-    ).isEqualTo(1)
-  }
+  @Nested
+  class Part2Test {
+    @Test
+    internal fun zeroIncreases() {
+      assertThat(
+        howManyIncreases(listOf(1, 1, 1, 1, 1, 1))
+      ).isEqualTo(0)
+    }
 
-  @Test
-  internal fun multipleIncreases() {
-    assertThat(
-      howManySubsequentIncreases(listOf(1, 1, 2, 3))
-    ).isEqualTo(2)
+    @Test
+    internal fun multipleIncreases() {
+      // listOf(1, 2, 2, 1, 2, 3)
+      // listOf(5, 5, 5, 6)
+      assertThat(
+        howManyIncreases(listOf(1, 2, 2, 1, 2, 3))
+      ).isEqualTo(1)
+    }
   }
 }
