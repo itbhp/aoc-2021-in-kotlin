7fa87ef719516c2e32f0fc1f39bce82cbbb90057
Paolo Sciarra <itbhps@gmail.com>
2021-12-03 13:38:30 +0100

    [aoc-2021] now we take into account aim

--- src/main/kotlin/it/twinsbrain/aco/kotlin/Day02.kt
+++ src/main/kotlin/it/twinsbrain/aco/kotlin/Day02.kt
@@ -1,6 +1,7 @@
 package it.twinsbrain.aco.kotlin
 
-import it.twinsbrain.aco.kotlin.Day02.Command.Companion.firstAttemptToMove
+import it.twinsbrain.aco.kotlin.Day02.Command.Companion.moveWithAim
+import it.twinsbrain.aco.kotlin.Day02.Command.Companion.moveWithoutAim
 import it.twinsbrain.aco.kotlin.Day02.move
 import it.twinsbrain.aco.kotlin.common.FileModule
 
@@ -11,16 +12,22 @@ fun main() {
 }
 
 object Day02 {
-  data class Position(val x: Int, val y: Int) {
+  data class Position(val x: Int, val y: Int, val aim: Int = 0) {
     fun moveForward(units: Int): Position = this.copy(x = this.x + units)
     fun moveDown(units: Int): Position = this.copy(y = this.y + units)
     fun moveUp(units: Int): Position = this.copy(y = this.y - units)
+    fun moveForwardWithAim(units: Int): Position = this.copy(x = this.x + units, y = this.y + this.aim * units)
+    fun moveDownWithAim(units: Int): Position = this.copy(aim = this.aim + units)
+    fun moveUpWithAim(units: Int): Position = this.copy(aim = this.aim - units)
   }
 
-  fun move(input: List<String>): Position {
-    return input.map(::toCommand)
-      .fold(Position(0, 0)) { position, command -> command.firstAttemptToMove(position) }
-  }
+  fun move(input: List<String>): Position =
+    input.map(::toCommand)
+      .fold(Position(0, 0)) { position, command -> command moveWithoutAim position }
+
+  fun moveWithAim(input: List<String>): Position =
+    input.map(::toCommand)
+      .fold(Position(0, 0, 0)) { position, command -> command moveWithAim position }
 
   private fun toCommand(input: String): Command {
     val (commandName, unit) = input.split(" ")
@@ -35,10 +42,16 @@ object Day02 {
 
   private sealed class Command {
     companion object {
-      fun Command.firstAttemptToMove(previousPosition: Position): Position = when (this) {
+      infix fun Command.moveWithoutAim(previousPosition: Position): Position = when (this) {
         is Forward -> previousPosition.moveForward(this.units)
         is Down -> previousPosition.moveDown(this.units)
         is Up -> previousPosition.moveUp(this.units)
+      }
+
+      infix fun Command.moveWithAim(previousPosition: Position): Position = when (this) {
+        is Forward -> previousPosition.moveForwardWithAim(this.units)
+        is Down -> previousPosition.moveDownWithAim(this.units)
+        is Up -> previousPosition.moveUpWithAim(this.units)
       }
     }
   }
@@ -46,6 +59,4 @@ object Day02 {
   private data class Forward(val units: Int) : Command()
   private data class Down(val units: Int) : Command()
   private data class Up(val units: Int) : Command()
-
-
 }
--- src/test/kotlin/it/twinsbrain/aco/kotlin/Day02Test.kt
+++ src/test/kotlin/it/twinsbrain/aco/kotlin/Day02Test.kt
@@ -2,49 +2,99 @@ package it.twinsbrain.aco.kotlin
 
 import it.twinsbrain.aco.kotlin.Day02.Position
 import it.twinsbrain.aco.kotlin.Day02.move
+import it.twinsbrain.aco.kotlin.Day02.moveWithAim
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class Day02Test {
 
-  @Test
-  internal fun `on no commands`() {
-    assertThat(move(emptyList())).isEqualTo(Position(0,0))
+  @Nested
+  class Part1Test {
+    @Test
+    internal fun `on no commands`() {
+      assertThat(move(emptyList())).isEqualTo(Position(0, 0))
+    }
+
+    @Test
+    internal fun `move forward`() {
+      assertThat(
+        move(
+          listOf(
+            "forward 4",
+            "forward 2"
+          )
+        )
+      ).isEqualTo(Position(6, 0))
+    }
+
+    @Test
+    internal fun `move down`() {
+      assertThat(
+        move(
+          listOf(
+            "down 4",
+            "down 2"
+          )
+        )
+      ).isEqualTo(Position(0, 6))
+    }
+
+    @Test
+    internal fun `move up`() {
+      assertThat(
+        move(
+          listOf(
+            "down 4",
+            "up 2"
+          )
+        )
+      ).isEqualTo(Position(0, 2))
+    }
   }
 
-  @Test
-  internal fun `move forward`() {
-    assertThat(
-      move(
-        listOf(
-          "forward 4",
-          "forward 2"
-        )
-      )
-    ).isEqualTo(Position(6,0))
-  }
+  @Nested
+  class Part2Test {
+    @Test
+    internal fun `on no commands`() {
+      assertThat(moveWithAim(emptyList())).isEqualTo(Position(0, 0, 0))
+    }
 
-  @Test
-  internal fun `move down`() {
-    assertThat(
-      move(
-        listOf(
-          "down 4",
-          "down 2"
+    @Test
+    internal fun `move down`() {
+      assertThat(
+        moveWithAim(
+          listOf(
+            "down 4",
+            "down 2"
+          )
         )
-      )
-    ).isEqualTo(Position(0,6))
-  }
+      ).isEqualTo(Position(0, 0, 6))
+    }
 
-  @Test
-  internal fun `move up`() {
-    assertThat(
-      move(
-        listOf(
-          "down 4",
-          "up 2"
+    @Test
+    internal fun `move up`() {
+      assertThat(
+        moveWithAim(
+          listOf(
+            "down 4",
+            "up 2"
+          )
         )
-      )
-    ).isEqualTo(Position(0,2))
+      ).isEqualTo(Position(0, 0, 2))
+    }
+
+    @Test
+    internal fun `move forward`() {
+      assertThat(
+        moveWithAim(
+          listOf(
+            "forward 4",
+            "down 2",
+            "forward 3"
+          )
+        )
+      ).isEqualTo(Position(7, 6, 2))
+    }
   }
 }
