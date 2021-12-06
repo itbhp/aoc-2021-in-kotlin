65a0cd63976748fbb8c2b7b69b3ff49c6d20dc5a
Paolo Sciarra <itbhps@gmail.com>
2021-12-06 19:12:40 +0100

    [aoc-2021] day3 added acceptance test

---
+++ src/test/kotlin/it/twinsbrain/aco/kotlin/Day03AcceptanceTest.kt
@@ -0,0 +1,21 @@
+package it.twinsbrain.aco.kotlin
+
+import it.twinsbrain.aco.kotlin.common.FileModule.readInput
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day03AcceptanceTest {
+
+  @Test
+  internal fun acceptanceTest() {
+    val day3Input = readInput("/inputs/day3.txt")
+    val rates = Day03.rates(day3Input)
+    val rating2 = Day03.ratings(day3Input)
+    val expectedPart1 = 741950
+    val expectedPart2 = 903810
+    assertThat(rates.gammaRate.value * rates.epsilonRate.value)
+      .isEqualTo(expectedPart1)
+    assertThat(rating2.oxygen.value * rating2.co2.value)
+      .isEqualTo(expectedPart2)
+  }
+}
