68a4fc8386c8279e5ca57fc69e8f9a3620fa9a99
Paolo Sciarra <itbhps@gmail.com>
2021-12-07 22:41:24 +0100

    [aoc-2021] day6 solved part 1

--- src/main/kotlin/it/twinsbrain/aoc/kotlin/Day06.kt
+++ src/main/kotlin/it/twinsbrain/aoc/kotlin/Day06.kt
@@ -1,5 +1,13 @@
 package it.twinsbrain.aoc.kotlin
 
+import it.twinsbrain.aoc.kotlin.Day06.countFish
+import it.twinsbrain.aoc.kotlin.common.FileModule
+
+fun main() {
+  val day6Input = FileModule.readInput("/inputs/day06.txt")
+  println(countFish(day6Input, 80))
+}
+
 object Day06 {
 
   fun countFish(input: List<String>, simulationDays: Int): Int {
@@ -9,11 +17,7 @@ object Day06 {
 
   fun worldAfter(input: List<String>, after: Int): World {
     val initialWorld = World(parse(input))
-    return (1..after).fold(initialWorld, ::simulate)
-  }
-
-  private fun simulate(previous: World, day: Int): World {
-    return World(previous.fishes.map { Fish(it.daysUntilReset - 1) })
+    return (1..after).fold(initialWorld) { previous, _ -> previous.newGeneration() }
   }
 
   fun parse(input: List<String>): List<Fish> = input
@@ -26,6 +30,18 @@ object Day06 {
 
   data class World(val fishes: List<Fish>) {
     fun fishCount() = fishes.size
+    fun newGeneration(): World {
+      val (toReset, others) = fishes.partition { it.daysUntilReset == 0 }
+      val resetWorld = World(
+        toReset.flatMap { listOf(Fish(6), Fish(8)) }
+      )
+      val movedForwardWorld = World(others).moveForward()
+      return resetWorld.union(movedForwardWorld)
+    }
+
+    private fun union(other: World): World = World(this.fishes + other.fishes)
+
+    private fun moveForward(): World = World(this.fishes.map { Fish(it.daysUntilReset - 1) })
   }
 
   data class Fish(var daysUntilReset: Int)
--- src/test/kotlin/it/twinsbrain/aoc/kotlin/Day06Test.kt
+++ src/test/kotlin/it/twinsbrain/aoc/kotlin/Day06Test.kt
@@ -1,7 +1,6 @@
 package it.twinsbrain.aoc.kotlin
 
 import it.twinsbrain.aoc.kotlin.Day06.Fish
-import it.twinsbrain.aoc.kotlin.Day06.World
 import it.twinsbrain.aoc.kotlin.Day06.parse
 import it.twinsbrain.aoc.kotlin.Day06.worldAfter
 import org.assertj.core.api.Assertions.assertThat
@@ -28,13 +27,30 @@ class Day06Test {
       worldAfter(
         listOf("3,4,3,1,2"),
         1
-      )
-    ).isEqualTo(
-      World(
-        listOf(
-          Fish(2), Fish(3), Fish(2), Fish(0), Fish(1)
-        )
-      )
+      ).fishes
+    ).containsExactlyInAnyOrder(
+      Fish(2),
+      Fish(3),
+      Fish(2),
+      Fish(0),
+      Fish(1)
+    )
+  }
+
+  @Test
+  internal fun `after 2 days`() {
+    assertThat(
+      worldAfter(
+        listOf("3,4,3,1,2"), // 1, 2, 3(2), 4 -> 0, 1, 2(2), 3 -> 0, 1(2), 2, 6, 8 -> 0(2), 1, 5, 6, 7, 8
+        2
+      ).fishes
+    ).containsExactlyInAnyOrder(
+      Fish(1),
+      Fish(2),
+      Fish(1),
+      Fish(6),
+      Fish(0),
+      Fish(8)
     )
   }
 }
