0604ad661cc4970e01176b9102065690b36f31e4
Paolo Sciarra <itbhps@gmail.com>
2021-12-07 09:26:09 +0100

    [aoc-2021] day3 refactoring

--- src/main/kotlin/it/twinsbrain/aoc/kotlin/Day03.kt
+++ src/main/kotlin/it/twinsbrain/aoc/kotlin/Day03.kt
@@ -14,9 +14,9 @@ typealias Bits = String
 fun main() {
   val day3Input = readInput("/inputs/day03.txt")
   val rates = rates(day3Input)
-  val rating2 = ratings(day3Input)
+  val ratings = ratings(day3Input)
   println("Part1: " + rates.gammaRate.value * rates.epsilonRate.value)
-  println("Part2: " + rating2.oxygen.value * rating2.co2.value)
+  println("Part2: " + ratings.oxygen.value * ratings.co2.value)
 }
 
 object Day03 {
