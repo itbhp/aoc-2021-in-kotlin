2b5950ef4eaf8df05cb131828c09d020d325b8bb
Paolo Sciarra <itbhps@gmail.com>
2021-12-07 17:21:26 +0100

    [aoc-2021] day5 attempt part1 solution

--- src/main/kotlin/it/twinsbrain/aoc/kotlin/Day05.kt
+++ src/main/kotlin/it/twinsbrain/aoc/kotlin/Day05.kt
@@ -1,9 +1,15 @@
 package it.twinsbrain.aoc.kotlin
 
+import it.twinsbrain.aoc.kotlin.Day05.Segment.Companion.segment
+import java.lang.Integer.min
+import kotlin.math.max
+
 object Day05 {
 
   fun howManyIntersections(input: List<String>): Int {
-    TODO()
+    val flatMap: List<Point> = segments(input).flatMap { it.points() }
+    val groupBy: Map<Point, List<Point>> = flatMap.groupBy { it }.filter { it.value.size > 1 }
+    return groupBy.keys.size
   }
 
   private val regexp = Regex("(\\d+),(\\d+) -> (\\d+),(\\d+)")
@@ -11,11 +17,42 @@ object Day05 {
   fun segments(input: List<String>): List<Segment> =
     input.map {
       val (x1, y1, x2, y2) = regexp.find(it)!!.destructured
-      Segment(Point(x1.toInt(), y1.toInt()), Point(x2.toInt(), y2.toInt()))
+      segment(x1.toInt(), y1.toInt(), x2.toInt(), y2.toInt())
     }
 
   data class Point(val x: Int, val y: Int)
   data class Segment(val start: Point, val end: Point) {
-    fun intersect(another: Segment): Point? = TODO()
+    companion object {
+      fun segment(x1: Int, y1: Int, x2: Int, y2: Int): Segment {
+        if (x2 < x1) {
+          return Segment(Point(x2, y2), Point(x1, y1))
+        }
+        return Segment(Point(x1, y1), Point(x2, y2))
+      }
+    }
+
+    fun points(): List<Point> {
+      if (end.y == start.y) {
+        return (start.x..end.x).map { Point(it, start.y) }.toList()
+      }
+      if (end.x == start.x) {
+        val miny = min(start.y, end.y)
+        val maxy = max(start.y, end.y)
+        return (miny..maxy).map { Point(start.x, it) }.toList()
+      }
+      val slope = (end.y - start.y) / (end.x - start.x)
+      val list = mutableListOf(start)
+      var previous = start
+      var reachedEnd = false
+      while (!reachedEnd) {
+        val next = Point(previous.x + 1, previous.y + slope)
+        if (next == end) {
+          reachedEnd = true
+        }
+        list.add(next)
+        previous = next
+      }
+      return list
+    }
   }
 }
--- src/test/kotlin/it/twinsbrain/aoc/kotlin/Day05Test.kt
+++ src/test/kotlin/it/twinsbrain/aoc/kotlin/Day05Test.kt
@@ -2,13 +2,57 @@ package it.twinsbrain.aoc.kotlin
 
 import it.twinsbrain.aoc.kotlin.Day05.Point
 import it.twinsbrain.aoc.kotlin.Day05.Segment
+import it.twinsbrain.aoc.kotlin.Day05.Segment.Companion.segment
+import it.twinsbrain.aoc.kotlin.Day05.howManyIntersections
 import it.twinsbrain.aoc.kotlin.Day05.segments
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class Day05Test {
+
+  @Test
+  internal fun `no intersections`() {
+    assertThat(
+      howManyIntersections(
+        listOf(
+          "0,1 -> 0,10",
+          "1,0 -> 10,0"
+        )
+      )
+    ).isEqualTo(0)
+  }
+
+  @Test
+  internal fun `one intersection`() {
+    assertThat(
+      howManyIntersections(
+        listOf(
+          "0,1 -> 0,10",
+          "0,1 -> 2,3"
+        )
+      )
+    ).isEqualTo(1)
+  }
+
+  @Test
+  internal fun `many intersection same point`() {
+    assertThat(
+      howManyIntersections(
+        listOf(
+          "0,1 -> 0,10",
+          "0,0 -> 0,1",
+          "0,1 -> 1,2",
+        )
+      )
+    ).isEqualTo(1)
+  }
 
   @Test
   internal fun `should adapt`() {
@@ -21,54 +65,29 @@ internal class Day05Test {
       )
     ).isEqualTo(
       listOf(
-        Segment(Point(556, 286), Point(341, 71)),
+        Segment(Point(341, 71), Point(556, 286)),
         Segment(Point(337, 201), Point(782, 646))
       )
     )
   }
 
   @Nested
-  @Disabled
-  inner class IntersectTest {
+  inner class ContainsTest {
 
-    @Test
-    internal fun `no intersection`() {
-      assertThat(
-        Segment(Point(0, 1), Point(0,10))
-          .intersect(
-            Segment(Point(1, 1), Point(1,10))
-          )
-      ).isNull()
-    }
-
-    @Test
-    internal fun `same start`() {
-      assertThat(
-        Segment(Point(0, 1), Point(0,10))
-          .intersect(
-            Segment(Point(0, 1), Point(1,10))
-          )
-      ).isEqualTo(Point(0, 1))
-    }
-
-    @Test
-    internal fun `same end`() {
-      assertThat(
-        Segment(Point(0, 1), Point(0,10))
-          .intersect(
-            Segment(Point(1, 1), Point(0,10))
-          )
-      ).isEqualTo(Point(0, 10))
-    }
-
-    @Test
-    internal fun `in the middle`() {
-      assertThat(
-        Segment(Point(2, 1), Point(2,10))
-          .intersect(
-            Segment(Point(0, 5), Point(3,5))
-          )
-      ).isEqualTo(Point(2, 5))
+    @ParameterizedTest
+    @ArgumentsSource(ContainsFixtures::class)
+    fun shouldWork(segment: Segment, points: List<Point>) {
+      assertThat(segment.points()).isEqualTo(points)
     }
   }
+}
+
+class ContainsFixtures : ArgumentsProvider {
+  override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> =
+    Stream.of(
+      Arguments.of(segment(0, 1, 0, 3), listOf(Point(0, 1), Point(0, 2), Point(0, 3))),
+      Arguments.of(segment(1, 0, 3, 0), listOf(Point(1, 0), Point(2, 0), Point(3, 0))),
+      Arguments.of(segment(0, 0, 2, 2), listOf(Point(0, 0), Point(1, 1), Point(2, 2))),
+      Arguments.of(segment(0, 1, 1, 10), listOf(Point(0, 1), Point(1, 10))),
+    )
 }
