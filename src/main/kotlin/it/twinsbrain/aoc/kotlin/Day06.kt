224df3e87cdc6165f8bf77ae821d84654e7b49e0
Paolo Sciarra <itbhps@gmail.com>
2021-12-07 21:53:15 +0100

    [aoc-2021] day6 added sample acceptance test

---
+++ src/main/kotlin/it/twinsbrain/aoc/kotlin/Day06.kt
@@ -0,0 +1,6 @@
+package it.twinsbrain.aoc.kotlin
+
+object Day06 {
+
+  fun countFish(input: List<String>, after: Int): Int = TODO()
+}
---
+++ src/test/kotlin/it/twinsbrain/aoc/kotlin/Day06AcceptanceTest.kt
@@ -0,0 +1,16 @@
+package it.twinsbrain.aoc.kotlin
+
+import it.twinsbrain.aoc.kotlin.Day06.countFish
+import it.twinsbrain.aoc.kotlin.common.FileModule.readInput
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class Day06AcceptanceTest {
+
+  @Test
+  internal fun acceptanceTest() {
+    val sample = readInput("/inputs/day06_sample.txt")
+    assertThat(countFish(sample, 18)).isEqualTo(26)
+    assertThat(countFish(sample, 80)).isEqualTo(5934)
+  }
+}
