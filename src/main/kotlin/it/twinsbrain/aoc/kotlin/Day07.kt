263888b917a9640b62b7e6e323a654ff940f6e42
Paolo Sciarra <itbhps@gmail.com>
2021-12-15 11:28:18 +0100

    [aoc-2021] day7 part1 solved

--- src/main/kotlin/it/twinsbrain/aoc/kotlin/Day07.kt
+++ src/main/kotlin/it/twinsbrain/aoc/kotlin/Day07.kt
@@ -1,7 +1,20 @@
 package it.twinsbrain.aoc.kotlin
 
+import kotlin.math.abs
+
 object Day07 {
   fun alignCrabsSameHorizontalPos(input: String): Int {
-    return 0
+    val crabsPos = input.split(",").map { it.trim().toInt() }.sorted()
+    val numberOfCrabs = crabsPos.size
+    return costToMoveToMedian(numberOfCrabs, crabsPos)
+  }
+
+  private fun costToMoveToMedian(numberOfCrabs: Int, crabsPos: List<Int>): Int {
+    val median = if (numberOfCrabs % 2 == 0) {
+      (crabsPos[(numberOfCrabs / 2) - 1] + crabsPos[numberOfCrabs / 2]) / 2
+    } else {
+      crabsPos[(numberOfCrabs / 2)]
+    }
+    return crabsPos.sumOf { abs(it - median) }
   }
 }
--- src/test/kotlin/it/twinsbrain/aoc/kotlin/Day07Test.kt
+++ src/test/kotlin/it/twinsbrain/aoc/kotlin/Day07Test.kt
@@ -9,4 +9,14 @@ internal class Day07Test {
   internal fun `1 crab`() {
     assertThat(alignCrabsSameHorizontalPos("1")).isEqualTo(0)
   }
+
+  @Test
+  internal fun `odd number of crabs`() {
+    assertThat(alignCrabsSameHorizontalPos("1, 3, 0")).isEqualTo(3)
+  }
+
+  @Test
+  internal fun `even number of crabs`() {
+    assertThat(alignCrabsSameHorizontalPos("1, 3, 0, 2")).isEqualTo(4)
+  }
 }
